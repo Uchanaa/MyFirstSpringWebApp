07248173a8c95e2b06a091c3940a807ddb475956
Uchana <uchaneishvili01@gmail.com>
2025-04-27 00:57:53 +0400

    first commit

---
+++ src/main/java/com/example/web/controller/MyFirstController.java
@@ -0,0 +1,19 @@
+package com.example.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyFirstController {
+
+    @GetMapping("/hello")
+    public String returnHelloMessage(){
+        return "Hello World;";
+    }
+
+    @GetMapping("/hello/{name}")
+    public String greetPerson(@PathVariable String name) {
+        return "Hello, " + name;
+    }
+}
